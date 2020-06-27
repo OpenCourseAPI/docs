83c321aa1be7a8fa98588c763977b19b343e9cec
Madhav Varshney <madhavarshney@gmail.com>
2020-06-27 02:48:03 -0700

    Experiment with API request examples in docs

---
+++ docs/.vuepress/components/api.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="api">
+    <p class="demo">
+      <span class="api-path">{{ msg }}</span>
+      <span class="description">Get a single course from the database</span>
+      <span class="spacer"></span>
+      <button class="action-button" v-on:click="sendRequest">Send API Request</button>
+    </p>
+    <pre><code class="json">{{ json }}</code></pre>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: '/fh/single',
+        json: '{ "message": "Nothing loaded yet... try clicking the button below!" }',
+      }
+    },
+    methods: {
+      sendRequest: async function () {
+        this.json = JSON.stringify(await (await fetch('https://opencourse.dev/fh/single?dept=CS&course=2A')).json(), undefined, 2)
+      }
+    },
+  }
+</script>
--- docs/guide/using-owlapi.md
+++ docs/guide/using-owlapi.md
@@ -1,3 +1,6 @@
 # Using OwlAPI
 
-Coming soon!
+This is an unofficial API that serves course data from Foothill DeAnza MyPortal to students wishing to use it. If you have a suggestion for what other FHDA data this API should serve, drop an issue on Github. OwlAPI currently lives at [opencourse.dev](https://opencourse.dev).
+
+<api />
+
